4aae190f83e05a47490f4e73fee2adc3a919e267
H.K <trucngonghinh2002@gmail.com>
2026-06-27 18:08:38 +0700

    feat: add DownloadMode constants

---
+++ DMFT.Core/Services/DownloadMode.cs
@@ -0,0 +1,9 @@
+namespace DMFT.Core.Services;
+
+public static class DownloadMode
+{
+    public const int Video = 0;
+    public const int AudioOnly = 1;
+    public const int AudioOriginOnly = 2;
+    public const int VideoAndAudioOrigin = 3;
+}
